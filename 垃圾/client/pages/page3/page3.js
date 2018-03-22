0a7692b506a3d3dfcac3484bc74fb5a5b14243e4
windingwind <33902321+windingwind@users.noreply.github.com>
2018-03-22 19:06:29 +0800

    垃圾的页面布局
    <p1>练习专用注意</p1>
    一股脑扔上来，就client文件夹里的有用，全部复制下来可能会报错

---
+++ 垃圾/client/pages/page3/page3.js
@@ -0,0 +1,77 @@
+// pages/page3/page3.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    city:"null_city",
+    map_info:{
+      str1:"mapinfo1",
+      str2:"mapinfo2"
+    }
+  },
+
+  set_position:function(){
+    //提交位置，刷新地图
+  },
+
+  select_city:function(){
+    //重新获取地理位置
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
